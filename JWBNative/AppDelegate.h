3f1a2b70b772a3f492bf6d56efc3beae7e2514e2
dkumar-csdcsystems <dkumar@csdcsystems.com>
2014-06-02 15:32:05 +0530

    Initial Commit

---
+++ JWBNative/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JWBNative
+//
+//  Created by CSDC on 6/2/14.
+//  Copyright (c) 2014 CSDC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JWBNative/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JWBNative
+//
+//  Created by CSDC on 6/2/14.
+//  Copyright (c) 2014 CSDC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
